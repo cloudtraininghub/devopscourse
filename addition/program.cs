7b7812bb7f35f56e45ec5aa05c1ccf16e8666bc4
Ravi Mishra <cloudtraininghub@gmail.com>
2025-02-09 09:45:41 +0530

    written c sharp code for addition of 2 number

---
+++ addition/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number1 = 10;
+        int number2 = 15;
+        int sum = number1 + number2;
+
+        Console.WriteLine("The sum of {0} and {1} is {2}.", number1, number2, sum);
+    }
+}
